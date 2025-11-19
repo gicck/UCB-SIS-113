8af33fbd8400dd306e5059e4c99e24a0bb3a25b8
Gonzalo Calle <gonzalo.calle.clavel@gmail.com>
2025-11-19 17:08:38 -0400

    Add demo project

---
+++ ProdNETCode/gatherly/src/Gatherly.Persistence/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using Gatherly.Domain.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Gatherly.Persistence;
+
+internal sealed class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _dbContext;
+    
+    public UnitOfWork(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IDbTransaction BeginTransaction()
+    {
+        var transaction = _dbContext.Database.BeginTransaction();
+
+        // _dbContext.Database.CurrentTransaction;
+        // _dbContext.Database.RollbackTransaction();
+        // _dbContext.Database.CommitTransaction();
+        return transaction.GetDbTransaction();
+    }
+
+    public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
